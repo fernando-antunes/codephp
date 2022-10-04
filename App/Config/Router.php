7fb18c8f312d5d989100e612be6d57a7c9a2865a
Fernando Antunes <fernando.cardoso@imbel.gov.br>
2022-10-04 11:09:53 -0300

    reogarnizando as pastas da core

--- App/Config/Router.php
+++ App/Config/Router.php
@@ -1,7 +1,7 @@
 <?php
 
 //Para chamar uma rota e necessario 4 parametros
-//1 - Tipo de requisição - GET, POST, PUT, DELETE
+//1 - Tipo de requisição - GET, POST
 //2 - O nome da rota, não obrigatorio ser o mesmo nome da FUNCTION da controller
 //3 - O nome da controller que esta sendo requisitada a FUNCTION
 //4 - O nome da FUNCTION que esta sendo requisitada
@@ -12,4 +12,4 @@ $this->get('/', 'HomeController', 'index');
 
 $this->get('inicio', 'HomeController', 'home');
 
-$this->get('testes', 'HomeController', 'teste');
+$this->get('teste', 'HomeController', 'teste');
